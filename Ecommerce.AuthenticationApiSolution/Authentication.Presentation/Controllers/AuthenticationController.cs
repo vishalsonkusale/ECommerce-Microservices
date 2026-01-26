a532d953b2eeead6d1ed73ee2b67833ee0af0c37
Cloud Computing <vishalsonkusale50@gmail.com>
2026-01-26 17:02:07 +0530

    Creating Endpoints for authentication

---
+++ Ecommerce.AuthenticationApiSolution/Authentication.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,44 @@
+﻿using Authentication.Application.Dtos;
+using Authentication.Application.Interfaces;
+using ECommerce.SharedLibrary.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderApi.Application.Dtos;
+
+namespace Authentication.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController(IUser user) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<Response>> Register(AppUserDto appUserDto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await user.Register(appUserDto);
+
+            return result.Flag ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<Response>> Register(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await user.Login(loginDto);
+
+            return result.Flag ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<GetUserDto>> GetUser(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            var result = await user.GetUser(id);
+
+            return result.Id > 0 ? Ok(result) : NotFound();
+        }
+    }
+}
--- Ecommerce.AuthenticationApiSolution/Authentication.Presentation/Program.cs
+++ Ecommerce.AuthenticationApiSolution/Authentication.Presentation/Program.cs
@@ -1,3 +1,4 @@
+using Authentication.infrastructure.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +8,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddInfrastructureService(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseInfrastructurePolicies();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
