52f9058bf192e6e183fc61926a7e6b4ffb23d465
Vishal <vishalsonkusale50@gmail.com>
2026-05-04 23:25:41 +0530

    Fixing

--- ECommerce.ProductApiSolution/ProductApi.Infrastructure/Data/ProductDbContextModelSnapshot.cs
+++ ECommerce.ProductApiSolution/ProductApi.Infrastructure/Data/ProductDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace ProductApi.Infrastructure.Data
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.23")
+                .HasAnnotation("ProductVersion", "8.0.26")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
